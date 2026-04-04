1e9d7afb5b631c85226c8af87c8c8f3bf85483cb
JohannesL2 <johannes.lindgren2002@gmail.com>
2026-04-04 21:26:51 +0200

    Simple KioskView setup using our menuData and cart

--- src/pages/KioskView.jsx
+++ src/pages/KioskView.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
+import menuData from '../data/data.json'
 
-export default function KioskView() {
+function KioskView({ socket }) {
+  const [cart, setCart] = useState([]);
+  
+  const addToCart = (item) => {
+    setCart([...cart, { ...item, cartId: Math.random() }]);
+  };
+
+  const handleCheckout = () => {
+    if (cart.length === 0) return;
+    const orderData = { id: Date.now(), items: cart };
+    socket.emit('new_order', orderData);
+    setCart([]);
+    alert("Thanks! Order was sent to /kitchen");
+  };
+
   return (
-    <div>KioskView</div>
+<div>
+    <div>
+        {menuData.map(item => (
+            <button key={item.id} onClick={() => addToCart(item)}>
+                <strong>{item.name}</strong><br/>{item.price} kr
+            </button>
+        ))}
+    </div>
+
+    <div>
+        {cart.map(item => <div key={item.cartId}>{item.name}</div>)}
+    </div>
+
+    {/* TOTAL AND PAYMENT */}
+    <div>
+        <h3>Total: {cart.reduce((sum, i) => sum + i.price, 0)} kr</h3>
+        <button
+            onClick={handleCheckout}
+            disabled={cart.length === 0}
+        >
+            Send to /Kitchen
+        </button>
+    </div>
+</div>
   )
 }
+
+export default KioskView;
